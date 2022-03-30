10d02438855a693e472846806ea0b1fc2b611558
Volha Ramanchyk <volha.ramanchyk@gmail.com>
2022-03-31 00:46:56 +0300

    js add solution task 52

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-// Declare the myGlobal variable below this line
-const myGlobal = 10;
-
-function fun1() {
-  oopsGlobal = 5;
-  // Assign 5 to oopsGlobal Here
-
-}
-
-// Only change code above this line
-
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
+function myLocalScope() {
+    // Only change code below this line
+    const myVar = '777';
+    console.log('inside myLocalScope', myVar);
   }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
-
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
