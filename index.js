0fa076ca5cbd13103331baa7af1a53c4fa11dbea
Volha Ramanchyk <volha.ramanchyk@gmail.com>
2022-04-05 23:31:46 +0300

    feat: add solution task 58

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-function nextInLine(arr, item) {
+function trueOrFalse(wasThatTrue) {
   // Only change code below this line
-  arr.push(item);
-  
-  return arr.shift();
+  if (wasThatTrue) {
+    return 'Yes, that was true';
+  } else {
+    return 'No, that was false';
+  }
+
+
+
   // Only change code above this line
+
 }
-
-// Setup
-const testArr = [1, 2, 3, 4, 5];
-
-// Display code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
